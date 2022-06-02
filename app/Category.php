694edd40d16febfe8a1d0179396b7a3574a974d3
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-06-02 09:37:38 +0200

    category.php and post.php

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
 
-    protected $table = 'Categories';
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,29 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
-
-    protected $fillable = ['title', 'content', 'slug'];
-
-
-
-    public static function convertToSlug($title)
+    protected $fillable = ['title', 'content', 'slug', 'category_id'];
+    public function category()
     {
-        $slugPrefix = Str::slug($title);
-        $slug = $slugPrefix;
-        $postFound = Post::where('slug', $slug)->first();
-        $counter = 1;
-
-        while ($postFound) {
-            $slug = $slugPrefix . '_' . $counter;
-            $counter++;
-            $postFound = Post::where('slug', $slug)->first();
-        }
-
-        return $slug;
+        return $this->belongsTo('App\Category');
     }
 }
