5b9acbf8f9f0daea032a53cdd0b29a4d08ab38bc
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-06-02 09:37:16 +0200

    add categoriesSeeder

---
+++ database/seeds/AddCategoriesSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Category;
+
+class AddCategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Bands', 'Duration', 'Title'];
+        foreach ($categories as $category) {
+            $newCategory_obj = new Category;
+            $newCategory_obj->name = $category;
+            $newCategory_obj->slug = Str::slug($category);
+            $newCategory_obj->save();
+        }
+    }
+}
